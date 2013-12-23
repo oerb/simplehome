7d5201603250646d76bf1a45e499c693a687368c
Oerb <bjoern@oerb.de>
2013-12-23 18:27:38 +0100

    New Structured not full tested Simplehome

--- simplehome/settings.py
+++ simplehome/settings.py
@@ -1,10 +1,13 @@
 # Django settings for simplehome project.
+import os
+# Images and other Files stored path settings
+ROOT_PATH = os.path.join(os.path.dirname(__file__), '..')
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('oerb', 'bjoern@oerb.de'),
 )
 
 MANAGERS = ADMINS
@@ -29,11 +32,11 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Berlin'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de'
 
 SITE_ID = 1
 
@@ -47,21 +50,20 @@ USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
-
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(ROOT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(ROOT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -107,11 +109,8 @@ ROOT_URLCONF = 'simplehome.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'simplehome.wsgi.application'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+#import os
+#TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), '..', 'templates').replace('\\','/'),)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -121,10 +120,51 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+    'endless_pagination',
+    'blog',
+    'menues',
+    'banner',
+    'tinymce',
 )
+
+# Template processors by Oerb
+TEMPLATE_CONTEXT_PROCESSORS = (
+        # default processors
+        # https://docs.djangoproject.com/en/dev/ref/settings/
+        "django.contrib.auth.context_processors.auth",
+        "django.core.context_processors.debug",
+        "django.core.context_processors.i18n",
+        "django.core.context_processors.media",
+        "django.core.context_processors.static",
+        "django.core.context_processors.tz",
+        "django.contrib.messages.context_processors.messages",
+        # infinit scrolling in templates
+        "django.core.context_processors.request",
+        # my custom_processor
+        "simplehome.custom_processors.menulist",
+        "simplehome.custom_processors.level2menulist",
+        "simplehome.custom_processors.bannerlist_right",
+        "simplehome.custom_processors.bannerlist_left",
+        "simplehome.custom_processors.footer",
+        "simplehome.custom_processors.metainfos_author",
+        "simplehome.custom_processors.metainfos_keywords",
+        "simplehome.custom_processors.metainfos_descriptions"
+)
+
+# Tinymce config
+# this line when installation not works and you need direct implementation
+# TINYMCE_JS_URL = '/media/js/tiny_mce/tiny_mce.js'
+TINYMCE_DEFAULT_CONFIG = {
+    'plugins': "table,spellchecker,paste,searchreplace",
+    'theme': "advanced",
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 10,
+}
+TINYMCE_SPELLCHECKER = True
+TINYMCE_COMPRESSOR = True
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
--- simplehome/urls.py
+++ simplehome/urls.py
@@ -1,17 +1,23 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'simplehome.views.home', name='home'),
-    # url(r'^simplehome/', include('simplehome.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # Wysiwyg Tool
+    url(r'^tinymce/', include('tinymce.urls')),
+    # Main Page Url
+    url(r'^$', 'blog.views.mainpage', name="mainpage"),
+    # Include for the App URL Files
+    url(r'^banner/', include('banner.urls')),
+    url(r'^blog/', include('blog.urls')),
+    url(r'^menues/', include('menues.urls')),
+    # Admin Foo
+    url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
